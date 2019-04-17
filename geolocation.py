0f2f1306f5c6119e6cca6c554df7dcbda2e7562d
salahuddin975 <salahuddin975@gmail.com>
2019-04-17 18:49:20 -0500

    add org name.

--- geolocation.py
+++ geolocation.py
@@ -1,6 +1,28 @@
 import geocoder
 from geopy.geocoders import Nominatim
 
+import pycurl
+from StringIO import StringIO
+
+
+def get_organization_name(ip_addr):
+    url = 'https://ipinfo.io/' + ip_addr
+
+    buffer = StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+
+    body = buffer.getvalue()
+
+    body_org = body[body.find("Organization</span>"):]
+    body_org = body_org[body_org.find("<span>"):]
+    org_name = body_org[body_org.find("<span>") + 6: body_org.find("</span>")]
+
+    return org_name
+
 
 def print_geolocation(route_info):
 
@@ -11,6 +33,8 @@ def print_geolocation(route_info):
             print hop_addr[0], "  *   *   *"
             continue
 
+        org_name = get_organization_name(addr)
+
         g = geocoder.ip(addr)
 
         try:
@@ -18,7 +42,8 @@ def print_geolocation(route_info):
             geo_addr = str(g.latlng[0]) + ', ' + str(g.latlng[1])
 
             location = geolocator.reverse(geo_addr)
-            print hop_addr[0]," ", addr, "-", location, " (" + str(g.latlng[0]) + ", " + str(g.latlng[1]) + ")"
+            print hop_addr[0],"  IP: " + str(addr) + ",  Org: " + str(org_name) + ",  Location: " + str(location) +\
+                             " (" + str(g.latlng[0]) + ", " + str(g.latlng[1]) + ")"
         except:
             print hop_addr[0]," ",addr, "not resolved!"
 
